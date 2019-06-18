bbf4e6367e2f8959db47f169e172b9cf57507af1
HassanBharu <hassan_7_8_6@hotmail.com>
2019-06-18 14:20:14 +0100

    changed to monitor video feed

--- test-openCv.py
+++ test-openCv.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 
 
-cap = cv2.VideoCapture('video.avi')
-cap.open('video.avi')
+cap = cv2.VideoCapture(0)
+
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
 while True:
-    ret, frame = cap.read()
-    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    ret, img = cap.read()
+    grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(grey,1.1,1)
     for(x,y,w,h) in cars:
-        cv2.rectangle(frame, (x,y),(x+w, y+h), (255,0,0), 2)
+        cv2.rectangle(img, (x,y),(x+w, y+h), (255,0,0), 2)
         
         
-    cv2.imshow('video', frame) 
+    cv2.imshow('img', img) 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
