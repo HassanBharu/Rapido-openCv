18645ef0b4ef38eb73647c8355f5c086b335f469
HassanBharu <hassan_7_8_6@hotmail.com>
2019-06-18 14:42:00 +0100

    testing PI camera

--- test-openCv.py
+++ test-openCv.py
@@ -8,15 +8,15 @@ print(cap)
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
 while True:
-    ret, img = cap.read()
+    ret, frame = cap.read()
     print(ret)
-    grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(grey,1.1,1)
     for(x,y,w,h) in cars:
-        cv2.rectangle(img, (x,y),(x+w, y+h), (255,0,0), 2)
+        cv2.rectangle(frame, (x,y),(x+w, y+h), (255,0,0), 2)
         
         
-    cv2.imshow('img', img) 
+    cv2.imshow('frame', frame) 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
